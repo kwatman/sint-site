bcf4df1d6abe00301a7f298be065693736026d4d
Yente Paternotte <yente.paternotte@hotmail.com>
2025-11-13 18:37:01 +0100

    feat: added umami script

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
+<head>
+    <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    @if(env('UMAMI_HOST') && env('UMAMI_ID'))
+        <script defer src="{{ env('UMAMI_HOST') }}" data-website-id="{{ env('UMAMI_ID') }}"></script>
+    @endif
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite('resources/js/app.ts')
     @inertiaHead
-  </head>
-  <body>
-    @inertia
-  </body>
+</head>
+<body>
+@inertia
+</body>
 </html>
